1dd014ce06ca96e8159f5ea3d3faaa31c0b60f6a
AhmedWaelSaid <138815865+AhmedWaelSaid@users.noreply.github.com>
2025-03-24 15:18:52 +0200

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-// src/App.jsx
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import Header from './components/Header';
 import { ThemeProvider } from './components/context/ThemeContext'; 
 import { MenuProvider } from './components/context/menuContext';
 import ResetPassword from './components/Auth/ResetPassword';
 import ForgotPassword from './components/Auth/ForgotPassword';
+import VerifyEmail from './components/Auth/VerifyEmail'; // استيراد VerifyEmail
+
 const HeroSection = lazy(() => import('./components/Find-Flight/HeroSection'));
 const FlightSearchForm = lazy(() => import('./components/Find-Flight/FlightSearchForm'));
 const TravelOffers = lazy(() => import('./components/Find-Flight/TravelOffers'));
@@ -13,25 +14,24 @@ const Footer = lazy(() => import('./components/Footer'));
 const Auth = lazy(() => import('./components/Auth/Auth'));
 const UserProfile = lazy(() => import('./components/UserProfile/UserProfile'));
 
-
 export default function App() {
   return (
     <div className="app">
-    <ThemeProvider>
-    <MenuProvider>
-      <Header />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<><HeroSection /><FlightSearchForm /><TravelOffers /></>} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/UserProfile" element={<UserProfile />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} /> {/* أضفنا الـ Route الجديد */}
-        <Route path="/reset-password" element={<ResetPassword />} /> {/* أضفنا الـ Route الجديد */}
-          
-        </Routes>
-      </Suspense>
-      <Footer />
-      </MenuProvider>
+      <ThemeProvider>
+        <MenuProvider>
+          <Header />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<><HeroSection /><FlightSearchForm /><TravelOffers /></>} />
+              <Route path="/auth" element={<Auth />} />
+              <Route path="/UserProfile" element={<UserProfile />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/reset-password" element={<ResetPassword />} />
+              <Route path="/verify-email" element={<VerifyEmail />} /> {/* إضافة المسار */}
+            </Routes>
+          </Suspense>
+          <Footer />
+        </MenuProvider>
       </ThemeProvider>
     </div>
   );
