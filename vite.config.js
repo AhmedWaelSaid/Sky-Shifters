313f404de533e80f5a3e90396907e307a9555f9b
AhmedWaelSaid <138815865+AhmedWaelSaid@users.noreply.github.com>
2025-05-05 12:56:47 +0300

    fix-errors

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'ui-vendor': ['react-icons', 'lucide-react', 'react-select'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
 })
