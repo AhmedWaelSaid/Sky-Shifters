e91c3188e619cb84688dddc8485b7232282f9a93
AhmedWaelSaid <138815865+AhmedWaelSaid@users.noreply.github.com>
2025-06-22 04:26:54 +0300

    Update BookingCard.jsx

--- src/pages/MyBookings/BookingCard.jsx
+++ src/pages/MyBookings/BookingCard.jsx
@@ -312,8 +312,8 @@ const BookingCard = ({ booking, onCancel, onPrintTicket, onCompletePayment, onDe
                   </div>
                 </div>
                 <div className={styles.totalPrice}>
-                  <span>Total amount required</span>
-                  <span>${(Number(booking.totalPrice) || Number(booking.price) || 0).toFixed(2)}</span>
+                  <span>Total amount required:</span>
+                  <span> ${(Number(booking.totalPrice) || Number(booking.price) || 0).toFixed(2)}</span>
                 </div>
               </div>
             </div>
